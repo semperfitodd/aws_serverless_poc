87eccca646ccfb066f1526278d098a04e5cb376d
semperfitodd <todd@bernsonfamily.com>
2023-03-12 09:57:32 -0700

    cleanup python

--- terraform/files/app.py
+++ terraform/files/app.py
@@ -2,7 +2,6 @@ import json
 
 
 def lambda_handler(event, context):
-    name = event.get('name', 'Unknown')
     message = f'This is your serverless application using API Gateway and Lambda.'
     return {
         'statusCode': 200,
